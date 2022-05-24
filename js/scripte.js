295241c452b02387291cca5bd2d6888f6b866559
Enzo <EnzoPiccolo7>
2022-05-24 15:02:01 +0200

    primo commit

---
+++ js/scripte.js
@@ -0,0 +1,6 @@
+/* in html creare delle card con immagini e descrizione prodotto e prezzo
+al click dobbiamo simulare un acquisto, abbiamo un budget di 100euro
+ogni volta che compriamo un prodotto, ci esce un alert che abbiamo acquistato e deve scalare il budget
+a meta budget deve uscire un alert che stiamo a meta,a fine budget deve uscire l alert che non abbiamo piu soldi.
+inserire un bottone su ricarica di 10euro 
+creare card in automatico tramite un Array */
